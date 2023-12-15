154554e25402a4a41d5a3dd63fa814189ae71b69
Bert-Ulrich Baumann <bubaumann@gmail.com>
2023-12-16 00:01:05 +0100

    fix contrast issues

--- blocks/profiles/profiles.js
+++ blocks/profiles/profiles.js
@@ -1,8 +1,8 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
-  block.classList.add(`columns-${cols.length}-cols`);
+  block.classList.add(`profiles-${cols.length}-cols`);
 
-  // setup image columns
+  // setup image profiles
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
@@ -10,7 +10,7 @@ export default function decorate(block) {
         const picWrapper = pic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
+          picWrapper.classList.add('profiles-img-col');
         }
       }
     });
